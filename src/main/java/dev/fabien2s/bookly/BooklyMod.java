7fa8f376c3223ea94c71fb0674f3bfa03bce1495
Fabien2S <18100602+Fabien2S@users.noreply.github.com>
2022-10-31 23:35:30 +0100

    Update mod details

---
+++ src/main/java/dev/fabien2s/bookly/BooklyMod.java
@@ -0,0 +1,16 @@
+package dev.fabien2s.bookly;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BooklyMod implements ModInitializer {
+
+    public static final String ID = "bookly";
+    public static final Logger LOGGER = LoggerFactory.getLogger(ID);
+
+    @Override
+    public void onInitialize() {
+    }
+
+}
